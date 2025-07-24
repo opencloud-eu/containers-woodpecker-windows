2a0e5aa94906c4bdd4a67ab0abf1894cac0fb8fd
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-07-24 17:39:58 +0200

    add parameters for single image builds and buildonly

--- build-all.ps1
+++ build-all.ps1
@@ -1,10 +1,25 @@
+Param(
+    [Parameter(
+        Position=0,
+        Mandatory=$false
+    )][string]$imageToBuild,
+    [Parameter(
+        Mandatory=$False
+    )][switch]$buildonly
+)
+
+### VARS
+# team/user name for the registry used to tag the images
 $registrypath = "opencloudeu"
 
+# registries to push the images to
 $registries = @(
     "docker.io",
     "quay.io"
 )
 
+# matrix with file-names target image names and tags, increment tags as needed
+# order is importent, the images are dependent on each other
 $matrix = @(
     @{
         name      = "windows-busybox"
@@ -71,14 +86,27 @@ $matrix = @(
     }
 )
 
+# iterate over the Dockerfile matrix
 $matrix | ForEach-Object {
+    # check if imageToBuild is specified and matches the current image
+    if ($imageToBuild -and $_.name -ne $imageToBuild) {
+        continue
+    }
     $name = $_.name
     $image = $_.imagename
     $tags = $_.tags
+    $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+    
+    # build image with name and latest-tag
     Write-Host "Building image: $image"
-    $arguments = '. -f ' + $( split-path -parent $MyInvocation.MyCommand.Definition ) + '"\' + "$name" + '.Dockerfile" -t "' + "$image" + ':latest"'
-    Invoke-Expression "docker build $arguments"
-    Write-Host "Adding tags: $image"
+    docker build $scriptPath -f "$scriptPath\$name.Dockerfile" -t "$image`:latest"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Build failed for image: $image"
+        exit $LASTEXITCODE
+    }
+    
+    # tag the image with the given tags for each registry
+    Write-Host "Adding tags to $image"
     $registries | ForEach-Object {
         $registry = $_
         $tags | ForEach-Object {
@@ -86,24 +114,30 @@ $matrix | ForEach-Object {
             $latest = "$image" + ":latest"
             $tagged = "$image" + ":" + "$tag"
             $regtag = "$registry/$tagged"
-            Write-Host "Tagging image $latest -> $tagged"
+            Write-Host "Tagging image $latest -> $regtag"
             docker tag "$latest" "$regtag"
         }
     }
 }
 
 # Push images to registries
-$registries | ForEach-Object {
-    $registry = $_
-    $matrix | ForEach-Object {
-        $image = $_.imagename
-        $tags = $_.tags
-        $tags | ForEach-Object {
-            $tag = $_
-            $tagged = "$image" + ":" + "$tag"
-            $regtag = "$registry/$tagged"
-            Write-Host "Pushing image $regtag"
-            docker push "$regtag"
+if (-not $buildonly) {
+    $registries | ForEach-Object {
+        $registry = $_
+        $matrix | ForEach-Object {
+            # check if imageToBuild is specified and matches the current image
+            if ($imageToBuild -and $_.name -ne $imageToBuild) {
+                continue
+            }
+            $image = $_.imagename
+            $tags = $_.tags
+            $tags | ForEach-Object {
+                $tag = $_
+                $tagged = "$image" + ":" + "$tag"
+                $regtag = "$registry/$tagged"
+                Write-Host "Pushing image $regtag"
+                docker push "$regtag"
+            }
         }
     }
 }
